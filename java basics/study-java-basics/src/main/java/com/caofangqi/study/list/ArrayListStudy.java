25a93033d924fa75e1da6321827dd8d40656d60a
caofangqi <caofangqi@outlook.com>
2019-03-01 20:27:19 +0800

    增加ArrayList的foreach 和for循环速度对比  测试

--- java basics/study-java-basics/src/main/java/com/caofangqi/study/list/ArrayListStudy.java
+++ java basics/study-java-basics/src/main/java/com/caofangqi/study/list/ArrayListStudy.java
@@ -1,11 +1,6 @@
 package com.caofangqi.study.list;
 
-import com.caofangqi.study.model.Student;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * 阅读ArrayList 源码
@@ -18,35 +13,44 @@ public class ArrayListStudy {
 
   public static void main(String[] args) {
 
-   ArrayList<Integer> arrayList = new ArrayList();
+    testForEachVsForSpeed(1000000);
 
-       arrayList.add(1);
-       arrayList.add(2);
-       arrayList.add(3);
-       arrayList.add(1);
-
-
-       arrayList.remove((Object)1);
-
-
-      arrayList.forEach(e-> {
-        System.out.println("----------------------------------");
-      });
-
-    Map<String,String> map=new HashMap<>();
-    map.forEach((e,d)->{
-
-    });
-
-   // System.out.println( arrayList.get(4));
-
-  /*  Integer integer=2147483647;
-    System.out.println(integer);*/
   }
 
 
-  public static void testRightMove(int a,int b){
-    System.out.println("a>>b:"+(a>>b));
+  public static void testRightMove(int a, int b) {
+    System.out.println("a>>b:" + (a >> b));
+  }
+
+  /**
+   * 测试forEach和for循环速度对比
+   *
+   * @author : 高冷的美男子
+   * @date : Created in 14:04 2019/2/26
+   */
+  public static void testForEachVsForSpeed(int count) {
+    ArrayList<Integer> arrayList = new ArrayList<>(100000);
+    //存数据到集合中
+    for (int i = 0; i < count; i++) {
+      arrayList.add(i);
+    }
+    //获取开始时间-纳秒
+    int s = arrayList.size();
+    long startTime = System.nanoTime();
+
+    for (int i = 0; i < s; i++) {
+      System.out.println(arrayList.get(i));
+    }
+    long forElapsedTime = System.nanoTime() - startTime;
+
+    startTime = System.nanoTime();
+    arrayList.forEach(e -> {
+      System.out.println(e);
+    });
+
+    long forEachElapsedTime = System.nanoTime() - startTime;
+    System.out.println(arrayList.size() + "条数据,for循环----消耗时间:" + forElapsedTime);
+    System.out.println(arrayList.size() + "条数据,forEach循环消耗时间:" + forEachElapsedTime);
   }
 
 }
