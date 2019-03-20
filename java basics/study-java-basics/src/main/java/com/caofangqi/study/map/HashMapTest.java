d463ebf08b9f2a98984aa05aa62460a66623dc76
FangQi Cao <caofangqi@outlook.com>
2019-03-20 23:21:37 +0800

    新增 HashMap 学习笔记

---
+++ java basics/study-java-basics/src/main/java/com/caofangqi/study/map/HashMapTest.java
@@ -0,0 +1,11 @@
+package com.caofangqi.study.map;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        HashMap<String,String> hashMap=new HashMap<>();
+        hashMap.put("hello", "hi");
+    }
+}
