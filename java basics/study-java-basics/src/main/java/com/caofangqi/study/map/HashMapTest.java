f8e93e2049a8a533bc2da768075879936a600d4f
FangQi Cao <caofangqi@outlook.com>
2019-03-21 23:53:04 +0800

    HashMap 学习中。。稍作修改

--- java basics/study-java-basics/src/main/java/com/caofangqi/study/map/HashMapTest.java
+++ java basics/study-java-basics/src/main/java/com/caofangqi/study/map/HashMapTest.java
@@ -1,11 +1,79 @@
 package com.caofangqi.study.map;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class HashMapTest {
 
     public static void main(String[] args) {
         HashMap<String,String> hashMap=new HashMap<>();
         hashMap.put("hello", "hi");
+    }
+}
+
+
+class MyHashMap implements Map {
+
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry> entrySet() {
+        return null;
     }
 }
--- study notes/java basics/map/HashMapStudyNotes.md
+++ study notes/java basics/map/HashMapStudyNotes.md
@@ -1,2 +1,11 @@
 # HashMap 学习笔记 (jdk 1.8)
 
+```
+This map usually acts as a binned (bucketed) hash table, but when bins get too large,they are transformed into bins of TreeNodes, each structured similarly to those in java.util.TreeMap.
+
+HashMap通常是一个不安全的哈希桶，但是当桶太大的时候，会变成类似TreeNodes 结构的树。
+
+```
+
+# 重要字段
+
