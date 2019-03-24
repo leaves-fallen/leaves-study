c214942f10848cb378f513cf030cb5ac5cde4e26
FangQi Cao <caofangqi@outlook.com>
2019-03-24 23:39:51 +0800

    增加 TreeMap 与 ConcurrentHashMap

---
+++ java-basics/study-java-basics/src/main/java/com/caofangqi/study/map/ConcurrentHashMapTest.java
@@ -0,0 +1,13 @@
+package com.caofangqi.study.map;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapTest {
+
+
+    public static void main(String []args){
+        ConcurrentHashMap concurrentHashMap=new ConcurrentHashMap();
+        concurrentHashMap.put(1, 1);
+
+    }
+}
---
+++ java-basics/study-java-basics/src/main/java/com/caofangqi/study/map/TreeMapTest.java
@@ -0,0 +1,11 @@
+package com.caofangqi.study.map;
+
+import java.util.TreeMap;
+
+public class TreeMapTest {
+
+    public static void main(String[]args){
+        TreeMap treeMap=new TreeMap();
+        treeMap.put(1, 2);
+    }
+}
