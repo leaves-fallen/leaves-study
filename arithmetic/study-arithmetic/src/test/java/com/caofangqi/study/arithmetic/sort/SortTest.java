af0caeba4b8a5e58752d79b7e5139265df0f729f
caofangqi <caofangqino1@gmail.com>
2019-05-25 23:04:31 +0800

    增加 耗时统计

--- arithmetic/study-arithmetic/src/test/java/com/caofangqi/study/arithmetic/sort/SortTest.java
+++ arithmetic/study-arithmetic/src/test/java/com/caofangqi/study/arithmetic/sort/SortTest.java
@@ -9,46 +9,55 @@ import org.junit.jupiter.api.Test;
  */
 public class SortTest {
 
-  Sort sort;
-  Integer[] nums = new Integer[]{879874, 5, 1374, 8674, 61, 35, 746, 745, 4165,
-      4, 9879, 7, 987, 979, 79, 789, 78, 645, 45, 13,45,35,635751,68486,132164 };
+    Sort sort;
+    Integer[] nums = new Integer[]{879874, 5, 1374, 8674, 61, 35, 746, 745, 4165,
+            4, 9879, 7, 987, 979, 79, 789, 78, 645, 45, 13, 45, 35, 635751, 68486, 132164};
 
 
-  public void test() {
+    public void test() {
+       long st=System.nanoTime();
+        sort.sort(nums);
+        long et=System.nanoTime();
+        sort.show(nums);
+        assertTrue(sort.isSorted(nums), "排序失败,数组不是升序的");
+        System.out.println(sort.toString()+" 耗时:"+(et-st)+" 纳秒");
+    }
 
-    sort.sort(nums);
-    sort.show(nums);
-    assertTrue(sort.isSorted(nums), "排序失败,数组不是升序的");
+    /**
+     * 冒泡排序测试
+     */
+    @Test
+    public void bubbleSortTest() {
+        sort = new BubbleSort();
+        test();
+    }
 
-  }
-
-  /**
-   * 冒泡排序测试
-   */
-  @Test
-  public void bubbleSortTest() {
-    sort = new BubbleSort();
-    test();
-  }
-
-  /**
-   * 选择排序测试
-   */
-  @Test
-  public void selectionSortTest() {
-    sort = new SelectionSort();
-    test();
-  }
+    /**
+     * 选择排序测试
+     */
+    @Test
+    public void selectionSortTest() {
+        sort = new SelectionSort();
+        test();
+    }
 
 
-  /**
-   * 插入排序测试
-   */
-  @Test
-  public void InsertionSortTest() {
-    sort = new InsertionSort();
-    test();
-  }
+    /**
+     * 插入排序测试
+     */
+    @Test
+    public void InsertionSortTest() {
+        sort = new InsertionSort();
+        test();
+    }
 
+    /*
+     * 希尔排序测试
+     * */
+    @Test
+    public void shellSort(){
+        sort=new ShellSort();
+        test();
+    }
 
 }
