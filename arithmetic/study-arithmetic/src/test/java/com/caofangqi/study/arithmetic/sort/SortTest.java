664ed46a170d9cd3459938203a91ec0c082f0d8c
caofangqi <caofangqino1@gmail.com>
2019-05-26 01:45:36 +0800

    修改 排序算法测试数组

--- arithmetic/study-arithmetic/src/test/java/com/caofangqi/study/arithmetic/sort/SortTest.java
+++ arithmetic/study-arithmetic/src/test/java/com/caofangqi/study/arithmetic/sort/SortTest.java
@@ -10,17 +10,19 @@ import org.junit.jupiter.api.Test;
 public class SortTest {
 
     Sort sort;
-    Integer[] nums = new Integer[]{879874, 5, 1374, 8674, 61, 35, 746, 745, 4165,
-            4, 9879, 7, 987, 979, 79, 789, 78, 645, 45, 13, 45, 35, 635751, 68486, 132164};
+    Integer[] nums = new Integer[]{879874, 5, 1374, 8674, 61, 35, 746, 745, 416,9345,
+            4, 9879, 7, 987, 979, 79, 789, 78, 645, 45, 13, 45, 35, 635751, 684,8567,
+            13216, 64, 879, 79, 64, 79, 5143, 1, 2893, 4, 5, 6, 9, 8, 7, 4, 52, 75,8,
+            964, 1, 3, 14, 6, 789, 794, 56, 1334, 68, 4465,78979,98445,4,898,188,1,3,6,9,0};
 
 
     public void test() {
-       long st=System.nanoTime();
+        long st = System.nanoTime();
         sort.sort(nums);
-        long et=System.nanoTime();
+        long et = System.nanoTime();
         sort.show(nums);
         assertTrue(sort.isSorted(nums), "排序失败,数组不是升序的");
-        System.out.println(sort.toString()+" 耗时:"+(et-st)+" 纳秒");
+        System.out.println(sort.toString() + " 耗时:" + (et - st) + " 纳秒");
     }
 
     /**
@@ -55,8 +57,8 @@ public class SortTest {
      * 希尔排序测试
      * */
     @Test
-    public void shellSort(){
-        sort=new ShellSort();
+    public void shellSort() {
+        sort = new ShellSort();
         test();
     }
 
