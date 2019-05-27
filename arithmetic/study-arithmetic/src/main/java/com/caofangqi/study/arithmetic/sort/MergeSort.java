468082a5498b0c8eb48fdaa59958412c8ff6407c
caofangqi <caofangqino1@gmail.com>
2019-05-27 23:51:19 +0800

    增加归并排序，及其测试类

--- arithmetic/study-arithmetic/src/main/java/com/caofangqi/study/arithmetic/sort/MergeSort.java
+++ arithmetic/study-arithmetic/src/main/java/com/caofangqi/study/arithmetic/sort/MergeSort.java
@@ -10,24 +10,41 @@ public class MergeSort implements Sort {
 
   @Override
   public void sort(Comparable[] arr) {
-
+   mergeSoft(arr);
   }
 
+
+  public Comparable[] sortOfReturn(Comparable[]arr){
+    return mergeSoft(arr);
+  }
   public Comparable[] mergeSoft(Comparable[]arr){
     if (arr.length<2)
       return arr;
     int mid=arr.length/2;
     Comparable []left= Arrays.copyOfRange(arr,0,mid);
     Comparable []right=Arrays.copyOfRange(arr,mid,arr.length);
-    return merge(MergeSort(left),MergeSort(right));
+    return merge(mergeSoft(left),mergeSoft(right));
   }
   public Comparable[] merge(Comparable[] left,Comparable[] right){
     Comparable []result=new Comparable[left.length+right.length];
-    for (int i = 0,l=0,r=0; i < result.length; i++) {
-       if ()
+    for (int i = 0,L=0,r=0; i < result.length; i++) {
+       if (L>=left.length){
+        result[i]=right[r++];
+       }else if (r>=right.length){
+         result[i]=left[L++];
+       }else if (less(left[L],right[r])){
+         result[i]=left[L++];
+       }else {
+         result[i]=right[r++];
+       }
 
     }
-    return null;
+    return result;
   }
 
+
+  @Override
+  public String toString() {
+    return "归并排序";
+  }
 }
--- arithmetic/study-arithmetic/src/test/java/com/caofangqi/study/arithmetic/sort/SortTest.java
+++ arithmetic/study-arithmetic/src/test/java/com/caofangqi/study/arithmetic/sort/SortTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 public class SortTest {
 
     Sort sort;
-    Integer[] nums = new Integer[]{879874, 5, 1374, 8674, 61, 35, 746, 745, 416,9345,
+    Comparable[] nums = new Integer[]{879874, 5, 1374, 8674, 61, 35, 746, 745, 416,9345,
             4, 9879, 7, 987, 979, 79, 789, 78, 645, 45, 13, 45, 35, 635751, 684,8567,
             13216, 64, 879, 79, 64, 79, 5143, 1, 2893, 4, 5, 6, 9, 8, 7, 4, 52, 75,8,
             964, 1, 3, 14, 6, 789, 794, 56, 1334, 68, 4465,78979,98445,4,898,188,1,3,6,9,0};
@@ -60,6 +60,17 @@ public class SortTest {
     public void shellSort() {
         sort = new ShellSort();
         test();
+    }
+
+    @Test
+    public void mergeSoft(){
+      MergeSort  sort=new MergeSort();
+        long st = System.nanoTime();
+       nums = sort.sortOfReturn(nums);
+        long et = System.nanoTime();
+        sort.show(nums);
+        assertTrue(sort.isSorted(nums), "排序失败,数组不是升序的");
+        System.out.println(sort.toString() + " 耗时:" + (et - st) + " 纳秒");
     }
 
 }
